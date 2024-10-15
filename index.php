baad56864733ee705620e60a1b50883261cd3b94
Ali N. Abadian <nagshrizali@gmail.com>
2024-10-15 10:41:25 +0330

    phpBlog

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en" dir='rtl'>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script defer src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+
+    <!-- Popper JS -->
+    <script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="" class="float-right">
+            </div>
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4">
+
+            </div>
+        </div>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <!-- Brand -->
+            <a class="navbar-brand" href="#">خانه</a>
+
+            <!-- Links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">فرهنگی</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">تاریخی</a>
+                </li>
+
+                <!-- Dropdown -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                        کامپیوتر
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">طراحی وب</a>
+                        <a class="dropdown-item" href="#">برنامه نویسی</a>
+                        <a class="dropdown-item" href="#">گرافیک</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <br />
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card mb-3">
+                    <div class="card-body">Basic card</div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-body">Basic card</div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-body">Basic card</div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-body">Basic card</div>
+                </div>
+            </div>
+
+
+            <div class="col-md-4">
+                <div class="card mb-3">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">Content</div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">Content</div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">Content</div>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
